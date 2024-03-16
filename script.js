aad16df1edaea619a516b9760c1001973e7875f8
kendy112 <kendyneilla@gmail.com>
2024-03-16 19:02:13 +0200

    Some changes were made

---
+++ script.js
@@ -0,0 +1,13 @@
+function redirectToRegister() {
+    window.location.href = "register.html";
+}
+
+function redirecttoLogin() {
+    window.location.href = "login.html";
+}
+function redirecttoDashboard() {
+    window.location.href = "dashboard.html"
+}
+function redirecttoTask() {
+    window.location.href = "task.html"
+}
