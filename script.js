0d86fec1d6d1b413464801ec6cfaad33e5ae112d
kendy112 <kendyneilla@gmail.com>
2024-05-13 14:04:10 +0300

    Some changes were made

--- script.js
+++ script.js
@@ -9,5 +9,44 @@ function redirecttoDashboard() {
     window.location.href = "dashboard.html"
 }
 function redirecttoTask() {
-    window.location.href = "task.html"
+    window.location.href = "do-task.html"
 }
+
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
